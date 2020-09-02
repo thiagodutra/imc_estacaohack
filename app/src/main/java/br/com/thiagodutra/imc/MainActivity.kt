563f6043bf832c0b08d33ec320babb54ab75c75a
Thiago Dutra <thiago.de.o.dutra@accenture.com>
2020-09-02 13:53:55 -0300

    Final project for Estacao's Hack

---
+++ app/src/main/java/br/com/thiagodutra/imc/MainActivity.kt
@@ -0,0 +1,55 @@
+package br.com.thiagodutra.imc
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fun validateInput(altura:String, peso:String): Boolean {
+            val lAltura = altura.split(".",",")
+            val lPeso = peso.split(".",",")
+
+            if(altura.isEmpty() || peso.isEmpty()) {
+                return false
+            }
+
+            if(lAltura.size <= 1 || lPeso.size <=1) {
+                return false
+            }
+            return true
+        }
+
+        btnMainCalcular.setOnClickListener{
+            val altura = edtMainAltura.text.toString().trim()
+            val peso = edtMainPeso.text.toString().trim()
+
+            if (altura.isEmpty() || peso.isEmpty()) {
+                if (altura.isEmpty()) {
+                    edtMainAltura.error = ("Campo obrigatório")
+                    edtMainAltura.requestFocus()
+                }
+                if (peso.isEmpty()) {
+                    edtMainPeso.error = "Campo obrigatório"
+                    edtMainPeso.requestFocus()
+                }
+            }
+            else if (!validateInput(altura, peso)){
+                Toast.makeText(this, "Valores de altura ou peso inválidos", Toast.LENGTH_SHORT).show()
+            } else {
+                val imc = peso.toDouble() / (altura.toDouble() * altura.toDouble())
+                val mIntent = Intent(this, ResultActivity::class.java)
+                mIntent.putExtra("IMC", imc)
+                startActivity(mIntent)
+                finish()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/br/com/thiagodutra/imc/ResultActivity.kt
@@ -0,0 +1,73 @@
+package br.com.thiagodutra.imc
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import androidx.core.util.toHalf
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_result.*
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val MUITO_ABAIXO_DO_PESO = 17
+        val ABAIXO_DO_PESO = 18.49
+        val PESO_NORMAL = 25.0
+        val ACIMA_DO_PESO= 30.0
+        val OBESIDADE_1 = 35.0
+        val OBESIDADE_2 = 40.0
+
+        fun Double.format(digits: Int) = "%.${digits}f".format(this)
+
+        val imc = intent.getDoubleExtra("IMC", 0.0)
+        val imgViewer = findViewById<ImageView>(R.id.imvResultImgInfo)
+        val defaultText = "Seu IMC é: ${imc.format(2)}"
+
+        if (imc == 0.0) {
+            txvResultMessage.text = "Erro inesperado, volte a tela de cálculo"
+            AlertDialog.Builder(this)
+                .setTitle("Atenção!")
+                .setMessage("Erro inesperado, volte a tela de cálculo")
+                .setPositiveButton("Voltar"){_,_ ->
+                    val mIntent = Intent(this, MainActivity::class.java)
+                    startActivity(mIntent)
+                    finish()
+                }
+                .setNegativeButton("Sair") { _, _ ->
+                    finishAffinity()
+                }
+                .setCancelable(false)
+                .create()
+                .show()
+        } else {
+            txvResultMessage.text = defaultText
+        }
+        if (imc >= 1 && imc < MUITO_ABAIXO_DO_PESO) {
+            imgViewer.setImageResource(R.drawable.imc_under_17)
+        } else if (imc >= MUITO_ABAIXO_DO_PESO && imc < ABAIXO_DO_PESO) {
+            imgViewer.setImageResource(R.drawable.imc_between_17_1849)
+        } else if (imc >= ABAIXO_DO_PESO && imc < PESO_NORMAL) {
+            imgViewer.setImageResource(R.drawable.imc_between_18_5_25)
+        } else if (imc >= PESO_NORMAL && imc < ACIMA_DO_PESO) {
+            imgViewer.setImageResource(R.drawable.imc_between_25_30)
+        } else if (imc >= ACIMA_DO_PESO && imc < OBESIDADE_1) {
+            imgViewer.setImageResource(R.drawable.imc_between_30_34)
+        } else if (imc >= OBESIDADE_1 && imc < OBESIDADE_2) {
+            imgViewer.setImageResource(R.drawable.imc_between_35_40)
+        } else if (imc >= OBESIDADE_2) {
+            imgViewer.setImageResource(R.drawable.imc_above_40)
+        }
+
+        btnResultRecalcular.setOnClickListener{
+            val mIntent = Intent(this, MainActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }
+
+    }
+}
